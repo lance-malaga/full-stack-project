a43a8ab5b598d4c8c6c42fa352a44881fbc2f0b1
lance-malaga <lmalaga@my.bcit.ca>
2024-03-22 01:46:26 -0700

    add furniture data

---
+++ public/data/furniture.js
@@ -0,0 +1,87 @@
+export const furniture_list = [
+    {
+        name: 'Tulip Side Table',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-1.svg',
+    },
+    {
+        name: 'Ceramic Mug Set',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-2.svg',
+    },
+    {
+        name: 'Curved Wall Mirror Shelf',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-3.svg',
+    },
+    {
+        name: 'Fun Curved Shelf',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-4.svg',
+    },
+    {
+        name: 'Ratan Side Table',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-5.svg',
+    },
+    {
+        name: 'Cozy Loveseat Sofa',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-6.svg',
+    },
+    {
+        name: 'Pastel Cloud Clock',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-7.svg',
+    },
+    {
+        name: 'Chrome Chair & Table Set',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-8.svg',
+    },
+    {
+        name: 'Sage Bubble Couch',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-9.svg',
+    },
+    {
+        name: 'Funky Carved Shelf',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-10.svg',
+    },
+    {
+        name: 'Arch Floor Mirror',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-11.svg',
+    },
+    {
+        name: 'Glass Table Lamp',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-12.svg',
+    },
+    {
+        name: 'Metal Storage Cabinet ',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-13.svg',
+    },
+    {
+        name: 'Tomato Lamp',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-14.svg',
+    },
+    {
+        name: 'Orange Tube Stool ',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-15.svg',
+    },
+    {
+        name: 'Modern Media Console',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-16.svg',
+    },
+    {
+        name: 'Standing Tulip Lamp',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        image: '/assets/furniture/image-17.svg',
+    },
+]
